5da02d52a6da7fca6bc0e2974a8e93db084c6419
zhyhjohn <879965956@qq.com>
2024-06-13 14:22:07 +0800

    chore: 更新eslint检查规则

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,6 +28,7 @@ module.exports = {
   rules: {
     "prettier/prettier": "error",
     "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
   },
 };
