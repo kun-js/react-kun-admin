20c6fafcfe98ebff74787d95cb15f515569d67ec
zhyhjohn <879965956@qq.com>
2024-06-13 10:19:54 +0800

    build: 新增打包配置

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,10 +2,10 @@ import MainLayout from "@/layout/index";
 import Analysis from "@/views/dashboard/analysis/index";
 import Workbench from "@/views/dashboard/workbench/index";
 import Login from "@/views/login/index";
-import { createHashRouter, Navigate } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <Navigate replace to="/dashboard" />, // 使用 Navigate 组件进行重定向，replace 属性表示替换历史记录
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { viteMockServe } from "vite-plugin-mock";
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,10 +11,60 @@ export default defineConfig({
     viteMockServe({
       mockPath: "./mock", //mock文件地址
       localEnabled: true, // 开发打包开关
-      logger: true, //是否在控制台显示请求日志
+      logger: false, //是否在控制台显示请求日志
       supportTs: true, //打开后，可以读取 ts 文件模块。 请注意，打开后将无法监视.js 文件
     }),
+    // 图片资源优化
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
   ],
+  build: {
+    minify: "terser", // 启用 terser 压缩
+    terserOptions: {
+      compress: {
+        drop_console: true, // 删除所有 console
+        drop_debugger: true, // 删除 debugger
+      },
+    },
+    rollupOptions: {
+      output: {
+        chunkFileNames: "js/[name]-[hash].js", // 引入文件名的名称
+        entryFileNames: "js/[name]-[hash].js", // 包的入口文件名称
+        assetFileNames: "[ext]/[name]-[hash].[ext]", // 资源文件像 字体，图片等
+        // 最小化拆分包
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
