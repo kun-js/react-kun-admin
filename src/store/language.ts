dbb6d11491c2e71feeb769364c7a72d2a9854b67
zhyhjohn <879965956@qq.com>
2024-07-05 18:12:38 +0800

    style: 调整整体样式

--- src/store/language.ts
+++ src/store/language.ts
@@ -3,23 +3,23 @@ import { persist } from "zustand/middleware";
 import i18n from "@/locales/index";
 
 interface LanguageType {
-    lang: string;
-    changeLanguage: (lang: string) => void;
+  lang: string;
+  changeLanguage: (lang: string) => void;
 }
 
 const useLanguageStore = create<LanguageType>()(
-    persist(
-        (set) => ({
-            lang: i18n.language,
-            changeLanguage: (lang: string) => {
-                i18n.changeLanguage(lang);
-                set({ lang });
-            },
-        }),
-        {
-            name: "languageStore",
-        }
-    )
+  persist(
+    (set) => ({
+      lang: i18n.language,
+      changeLanguage: (lang: string) => {
+        i18n.changeLanguage(lang);
+        set({ lang });
+      },
+    }),
+    {
+      name: "languageStore",
+    }
+  )
 );
 
 export default useLanguageStore;
