436ca16dd773e875ec3de1231177169bc82ea707
zhyhjohn <879965956@qq.com>
2024-07-11 16:26:52 +0800

    chore: 配置代码检查规则

---
+++ .stylelintrc.cjs
@@ -0,0 +1,125 @@
+module.exports = {
+  // 继承推荐规范配置
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-recess-order",
+    "stylelint-config-prettier",
+    "stylelint-config-prettier-scss",
+  ],
+  // 指定不同文件对应的解析器
+  overrides: [
+    {
+      files: ["**/*.{html}"],
+      customSyntax: "postcss-html",
+    },
+    {
+      files: ["**/*.{css,scss}"],
+      customSyntax: "postcss-scss",
+    },
+  ],
+  ignoreFiles: ["**/*.js", "**/*.jsx", "**/*.tsx", "**/*.ts", "**/*.json", "**/*.md", "**/*.yaml"],
+  // 自定义规则
+  rules: {
+    "import-notation": "string", // 指定导入CSS文件的方式("string"|"url")
+    "selector-class-pattern": null, // 选择器类名命名规则
+    "custom-property-pattern": null, // 自定义属性命名规则
+    "keyframes-name-pattern": null, // 动画帧节点样式命名规则
+    "no-descending-specificity": null, // 允许无降序特异性
+    "no-empty-source": null, // 允许空样式
+    // 允许 global 、export 、deep伪类
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global", "export", "deep"],
+      },
+    ],
+    // 允许未知属性
+    "property-no-unknown": [
+      true,
+      {
+        ignoreProperties: [],
+      },
+    ],
+    // 允许未知规则
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["apply", "use"],
+      },
+    ],
+    "order/properties-order": [
+      "position",
+      "top",
+      "right",
+      "bottom",
+      "left",
+      "z-index",
+      "display",
+      "justify-content",
+      "align-items",
+      "float",
+      "clear",
+      "overflow",
+      "overflow-x",
+      "overflow-y",
+      "padding",
+      "padding-top",
+      "padding-right",
+      "padding-bottom",
+      "padding-left",
+      "margin",
+      "margin-top",
+      "margin-right",
+      "margin-bottom",
+      "margin-left",
+      "width",
+      "min-width",
+      "max-width",
+      "height",
+      "min-height",
+      "max-height",
+      "font-size",
+      "font-family",
+      "text-align",
+      "text-justify",
+      "text-indent",
+      "text-overflow",
+      "text-decoration",
+      "white-space",
+      "color",
+      "background",
+      "background-position",
+      "background-repeat",
+      "background-size",
+      "background-color",
+      "background-clip",
+      "border",
+      "border-style",
+      "border-width",
+      "border-color",
+      "border-top-style",
+      "border-top-width",
+      "border-top-color",
+      "border-right-style",
+      "border-right-width",
+      "border-right-color",
+      "border-bottom-style",
+      "border-bottom-width",
+      "border-bottom-color",
+      "border-left-style",
+      "border-left-width",
+      "border-left-color",
+      "border-radius",
+      "opacity",
+      "filter",
+      "list-style",
+      "outline",
+      "visibility",
+      "box-shadow",
+      "text-shadow",
+      "resize",
+      "transition",
+    ],
+  },
+};
